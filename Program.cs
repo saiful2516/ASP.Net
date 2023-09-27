737651ab09a1608d7f6a6a9d093c4c0b30f8c649
Saiful Islam <72754002+saiful2516@users.noreply.github.com>
2023-09-27 20:43:48 +0200

    Taking User Input, sum,  Value Type conversion.

--- Program.cs
+++ Program.cs
@@ -2,15 +2,24 @@
 {
     private static void Main(string[] args)
     {
-        string messege = "Hello World";
-        Console.WriteLine(messege);
-        Console.WriteLine("Hello, World!");
-       
 
-        int number = 10;
-        int result = number + 20;
-        Console.WriteLine("Result :" + result);
-        Console.ReadLine();
+        //user input
+        //Explicit Conversion
+        Console.WriteLine("Enter your first Number:");
+       
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter your Second Number:");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        //string messege = Console.ReadLine(); comment shor key: ctrl+k+c
+
+        int result = firstNumber + secondNumber;
+
+        Console.WriteLine("Output is:" +result);
+        char c = 'A';
+        int acii = c; // find the ascii value
+        Console.WriteLine(acii);
+
         Console.ReadKey();
     }
 }
