9d8f9ac37df38d0484f3961bdd6ef295ee3353c5
Saiful Islam <72754002+saiful2516@users.noreply.github.com>
2023-10-05 18:09:28 +0200

    Tuple
    When you want to return multiple values from a method without using ref or out parameters.
    1. To pass multiple values to a method through a single parameter.
    To hold a database record or some values temporarily without creating a separate class.
    2.Tuple Limitations:
    The Tuple is a reference type and not a value type. 
    The Tuple is limited to include eight elements. You need to use nested tuples if you need to store more elements.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿
+using System;
+using System.Reflection;
 using System.Collections.Generic;
+using System.Text;
 using System.Linq;
 
 
@@ -8,47 +10,28 @@ using System.Linq;
 internal class Program
 {
     private static void Main(string[] args)
-    {   //Dictionary type fixed
-        Dictionary<int, string> aDictionary = new Dictionary<int, string>();
-        aDictionary.Add(1, "phoenix");
-        aDictionary.Add(2, "Honda");
-        aDictionary.Add(3, "Hero");
-        string outPut;
-        if(aDictionary.TryGetValue(5, out outPut))  // Bolean type output
-        {
-            Console.WriteLine("find data:" + outPut);
-        }
-        else
-        {
-            Console.WriteLine("Not found");
-        }
-        foreach(var data in aDictionary)
-        {
-            Console.WriteLine("Id:" + data.Key +" value:" +data.Value);
-        }
-        //Hashtable dynamic type
-        Hashtable aHashtable = new Hashtable();
-        aHashtable.Add(1, "FSV");
-        aHashtable.Add(2, "Two");
-        aHashtable.Add(3, 4);
-        aHashtable.Add(4, 35.5);
-        aHashtable.Add(5, 30.5);
-        aHashtable.Add(6, 10);
-        aHashtable["1"] = "Fisrt entry";
-        foreach(var data in aHashtable.Values)
-        {
-            Console.WriteLine(data);
-        }
-        foreach (var data in aHashtable.Keys)
-        {
-            Console.WriteLine(data);
-        }
-        if (aHashtable.Contains("1"))
-        {
-            Console.WriteLine("Data found");
-        }
+    {
+       var aPerson = Tuple.Create(1, "Dept:Computer Sciences", "Type: Employee");
+        Console.WriteLine(aPerson.Item1);
+        Console.WriteLine(aPerson.Item2);
+        Console.WriteLine(aPerson.Item3);
+
+        var numbers = Tuple.Create(1, 2, Tuple.Create(3, 4, 5, 6, 7, 8), 9, 10, 11, 12, 13);
+        Console.WriteLine(numbers.Item1); // returns 1
+        Console.WriteLine(numbers.Item2); // returns 2
+        Console.WriteLine(numbers.Item3); // returns (3, 4, 5, 6, 7,  8)
+        Console.WriteLine(numbers.Item3.Item1); // returns 3
+        Console.WriteLine(numbers.Item4); // returns 9
+        Console.WriteLine(numbers.Rest.Item1); //returns 13
+
+        var person = Tuple.Create(1, "Steve", "Jobs");
+        DisplayTuple(person);
         Console.ReadKey();
-        
-       
+    }
+    static void DisplayTuple(Tuple<int,string,string> person)
+    {
+        Console.WriteLine($"Id = {person.Item1}");
+        Console.WriteLine($"First Name = {person.Item2}");
+        Console.WriteLine($"Last Name = {person.Item3}");
     }
 }
